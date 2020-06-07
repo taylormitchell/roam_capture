a2e7b047d52164853c5d6ce19bc073592e16451c
Taylor Mitchell <taylor.mitchell@ibm.com>
2020-06-07 12:05:19 -0400

    Add tweet capture to captureQuote.js

--- captureQuote.js
+++ captureQuote.js
@@ -1,3 +1,20 @@
+// Helper functions
+tag_user = function(tweet) {
+    return tweet.replace(/^(\(\d*\))?\s?(.*) on Twitter: (".*") \/ Twitter/,"[[$2]] on Twitter: $3");
+};
+add_tags =  function(tweet, tags=["#Quote", "#Tweet"]) {
+    return tweet + " " + tags.join(" ")
+}
+add_source_as_child = function(tweet, url) {
+    return `- ${tweet}\n  - source:: ${url}`
+}
+roamify = function(tweet, url) {
+    tweet = tag_user(tweet);
+    tweet = add_tags(tweet);
+    tweet = add_source_as_child(tweet, url);
+    return tweet
+}
+
 //source:: https://stackoverflow.com/a/18455088
 
 //Create area for the text
@@ -5,8 +22,16 @@ var copyFrom = document.createElement("textarea");
 document.body.appendChild(copyFrom);
 
 //Create the text string you want to copy
+var base_url = window.location.origin;
 var selection = document.getSelection().toString();
-var text = `${selection} #Quote [source](${document.URL})`;
+
+if (base_url==="https://twitter.com" && selection==="") {
+    var tweet = document.title;
+    var url = document.URL;
+    var text = roamify(tweet, url);
+} else {
+    var text = `${selection} #Quote [source](${document.URL})`;
+}
 
 //Add the text string to the text area and copy it
 copyFrom.textContent = text;
